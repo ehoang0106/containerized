9a5de8a2d421ed2b9e2f637087ae2bcd6471e168
ehoang0106 <khoahoang1698@gmail.com>
2025-03-14 16:05:01 -0700

    inital init

---
+++ terraform/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-state-khoa-hoang"
+    key    = "terraform-state-aws-rds"
+    region = "us-west-1"
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
