d9dfefa732f274a56b8ce5afdef6883567c27f33
Khoa Hoang <khoahoang1698@gmail.com>
2025-03-25 15:11:25 -0700

    add user data

---
+++ terraform/infra/_data.tf
@@ -0,0 +1,11 @@
+data "aws_security_group" "orbwatch_sg" {
+  name = "orbwatch_sg"
+}
+
+data "local_file" "orbwatch_sg" {
+  filename = "${path.module}/userdata.tftpl"
+}
+
+data "template_file" "user_data" {
+  template = data.local_file.orbwatch_sg.content
+}
--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -25,3 +25,35 @@ resource "aws_db_instance" "orbwatch_db" {
     Name = "orbwatch-db"
   }
 }
+
+# launch template
+
+resource "aws_launch_template" "orbwatch_launch_template" {
+  name = "orbwatch-launch-template"
+  image_id = "ami-01eb4eefd88522422"
+  instance_type = "t3.micro"
+  key_name = "orb-key"
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups = [aws_security_group.orbwatch_sg.id]
+  }
+
+  iam_instance_profile {
+    name = "ecsInstanceRole"
+  }
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = 30
+      volume_type = "gp2"
+    }
+  }
+
+  user_data = base64decode(data.template_file.user_data.rendered)
+
+  tags = {
+    Name = "orbwatch-launch-template"
+  }
+}
