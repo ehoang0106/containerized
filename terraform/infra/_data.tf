e6c5d2f73df2aeeb4c5e3f85741f6a443d9f3c3d
ehoang0106 <khoahoang1698@gmail.com>
2025-03-25 23:46:54 -0700

    update data variable

--- terraform/infra/_data.tf
+++ terraform/infra/_data.tf
@@ -1,7 +1,9 @@
 data "aws_security_group" "orbwatch_sg" {
   name = "orbwatch_sg"
 }
-
+data "aws_security_group" "db_sg" {
+  name = "db_sg"
+}
 data "local_file" "orbwatch_sg" {
   filename = "${path.module}/userdata.tftpl"
 }
@@ -13,3 +15,25 @@ data "template_file" "user_data" {
 data "aws_ami_role" "ecs_execution_role" {
   name = "ecsTaskExecutionRole"
 }
+
+data "aws_vpc" "orbwatch_vpc" {
+  filter {
+    name = "tag:Name"
+    values = ["orbwatch_vpc"]
+  }
+}
+
+data "aws_subnet" "orbwatch_subnet1" {
+  filter {
+    name = "tag:Name"
+    values = ["orbwatch_subnet1"]
+  }
+}
+
+data "aws_subnet" "orbwatch_subnet2" {
+  filter {
+    name = "tag:Name"
+    values = ["orbwatch_subnet2"]
+  }
+}
+
