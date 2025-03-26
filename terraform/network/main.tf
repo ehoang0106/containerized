d7c86cb2658be8f461e6f57bbe36447cd6274275
Khoa Hoang <khoahoang1698@gmail.com>
2025-03-26 08:19:37 -0700

    fix typo cidr block subnet2

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "orbwatch_subnet1" {
 
 resource "aws_subnet" "orbwatch_subnet2" {
   vpc_id = aws_vpc.orbwatch_vpc.id
-  cidr_block = "10.0.0.1/24"
+  cidr_block = "10.0.1.0/24"
   availability_zone = "us-west-1c"
   tags = {
     Name = "orbwatch-subnet2"
