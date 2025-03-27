ab1f1f2fc3de93db65068ef54ac40382b15fb110
Khoa Hoang <khoahoang1698@gmail.com>
2025-03-27 14:50:27 -0700

    add certificate

--- terraform/infra/_vars.tf
+++ terraform/infra/_vars.tf
@@ -12,3 +12,7 @@ variable "db_password" {
   type        = string
   default = "Xzkiller1698!"
 }
+
+variable "cert_arn" {
+  default = "arn:aws:acm:us-west-1:706572850235:certificate/eac6e688-8e0c-4545-a122-d67d1d7bf04a"
+}
